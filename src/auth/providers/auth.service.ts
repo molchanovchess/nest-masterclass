44a722cbbe15169f1edb1e78dc09b11b55ca295e
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-08-05 12:13:55 +0300

    DI

---
+++ src/auth/providers/auth.service.ts
@@ -0,0 +1,17 @@
+import { Inject, Injectable, forwardRef } from '@nestjs/common';
+import { UsersService } from 'src/users/providers/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+  public login(email: string, password: string) {
+    const user = this.usersService.findOneById('1234');
+    return 'SAMPLE_TOKEN';
+  }
+  public isAuth() {
+    return true;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -3,30 +3,28 @@ import {
   Controller,
   DefaultValuePipe,
   Get,
-  Headers,
-  Ip,
   Param,
   ParseIntPipe,
   Patch,
   Post,
   Query,
-  ValidationPipe,
 } from '@nestjs/common';
-import { Request } from 'express';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { GetUsersParamDto } from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-user.dto';
+import { UsersService } from './providers/users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
   @Get('/:id?')
   public getUsers(
     @Param() getUsersParamDto: GetUsersParamDto,
     @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
     @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
   ) {
-    console.log('getUsersParamDto:', getUsersParamDto);
-    return 'You sent a GET request to users endpoint';
+    return this.usersService.findAll(getUsersParamDto, limit, page);
   }
   @Post()
   public createUsers(@Body() createUserDto: CreateUserDto) {
@@ -39,10 +37,10 @@ export class UsersController {
   //     return 'You sent a POST request to users endpoint';
   //   }
 
-    @Patch()
-    public patchUser(@Body() patchUserDto: PatchUserDto) {
-      return patchUserDto;
-    }
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUserDto) {
+    return patchUserDto;
+  }
 
   //   @Get()
   //   public getUsers() {
