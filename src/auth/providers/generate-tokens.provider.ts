6725feb28b41200afe188bef0bbfe643f4a23986
alexmolchanov <oleksiimolchanov@bitbucket.org>
2024-08-16 20:57:14 +0300

    refresh tokens

---
+++ src/auth/providers/generate-tokens.provider.ts
@@ -0,0 +1,56 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import jwtConfig from '../config/jwt.config';
+import { ConfigType } from '@nestjs/config';
+import { User } from 'src/users/user.entity';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
+
+@Injectable()
+export class GenerateTokensProvider {
+  constructor(
+    /**
+     * Injecting jwtService
+     */
+    private readonly jwtService: JwtService,
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+  ) {}
+
+  public async signToken<T>(userId: number, expiresIn: number, payload?: T) {
+    return await this.jwtService.signAsync(
+      {
+        sub: userId,
+        ...payload,
+      },
+      {
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+        secret: this.jwtConfiguration.secret,
+        expiresIn,
+      },
+    );
+  }
+
+  public async generateTokens(user: User) {
+    const [accessToken, refreshToken] = await Promise.all([
+      // Generate Access Token
+      this.signToken<Partial<ActiveUserData>>(
+        user.id,
+        this.jwtConfiguration.accessTokenTtl,
+        {
+          email: user.email,
+        },
+      ),
+
+      // Generate Refresh Token
+      this.signToken(user.id, this.jwtConfiguration.refreshTokenTtl),
+    ]);
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
+}
---
+++ src/auth/providers/refresh-tokens.provider.ts
@@ -0,0 +1,58 @@
+import {
+  Inject,
+  Injectable,
+  UnauthorizedException,
+  forwardRef,
+} from '@nestjs/common';
+import { RefreshTokenDto } from '../dtos/refresh-token.dto';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigType } from '@nestjs/config';
+import jwtConfig from '../config/jwt.config';
+import { GenerateTokensProvider } from './generate-tokens.provider';
+import { UsersService } from 'src/users/providers/users.service';
+import { ActiveUserData } from '../interfaces/active-user-data.interface';
+
+@Injectable()
+export class RefreshTokensProvider {
+  constructor(
+    /**
+     * Inject jwtService
+     */
+    private readonly jwtService: JwtService,
+
+    /**
+     * Inject jwtConfiguration
+     */
+    @Inject(jwtConfig.KEY)
+    private readonly jwtConfiguration: ConfigType<typeof jwtConfig>,
+    /**
+     * Inject generateTokensProvider
+     */
+    private readonly generateTokensProvider: GenerateTokensProvider,
+    /**
+     * Inject UsersService
+     */
+    @Inject(forwardRef(() => UsersService))
+    private readonly usersService: UsersService,
+  ) {}
+  public async refreshTokens(refreshTokenDto: RefreshTokenDto) {
+    try {
+      // verify the refresh token using jwtService
+      const { sub } = await this.jwtService.verifyAsync<
+        Pick<ActiveUserData, 'sub'>
+      >(refreshTokenDto.refreshToken, {
+        secret: this.jwtConfiguration.secret,
+        audience: this.jwtConfiguration.audience,
+        issuer: this.jwtConfiguration.issuer,
+      });
+
+      // Fetch user from the database
+      const user = await this.usersService.findOneById(sub);
+
+      // Generate the tokens
+      return await this.generateTokensProvider.generateTokens(user);
+    } catch (error) {
+      throw new UnauthorizedException();
+    }
+  }
+}
